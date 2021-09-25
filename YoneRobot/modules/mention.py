0a4f190dd1735d49b6dc369c1034b6b34045f4ac
devilroogan <85479917+devilroogan@users.noreply.github.com>
2021-09-25 17:37:22 +0530

    Update mention.py

--- YoneRobot/modules/mention.py
+++ YoneRobot/modules/mention.py
@@ -59,7 +59,7 @@ async def mentionall(event):
       usrnum += 1
       usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
       if usrnum == 5:
-        await client.send_message(event.chat_id, usrtxt, reply_to=(msg,mention))
+        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
         await asyncio.sleep(2)
         usrnum = 0
         usrtxt = ""
